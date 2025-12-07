fd0a3302e96b018876770cc6a7a83b6d00ea739e
tofipie <62646269+tofipie@users.noreply.github.com>
2025-12-07 04:43:01 -0800

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,6 @@
+def get_data_files():
+    data_files = []
+    for dirname, _, filenames in os.walk("images"):
+        for filename in filenames:
+            data_files.append(os.path.join(filename))
+    return data_files
